82ccc51c4a4c8d77fed9bb5d209b0fcd9dbc1d23
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2024-04-03 09:30:38 -0300

    Configurado

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path'); // Import path module
+
+const app = express();
+const port = 3000;
+
+// Allow all cross-origin requests
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// -------------------------------------------- ENDPOINTS --------------------------------------------
+
+/****************************************
+ * Business
+****************************************/
+
+app.get('/players', async (req, res) => {
+  try{
+    
+    }catch(e){
+      res.status(500).send({'error': 'Internal server error'})
+    }
+})
+
+
+
+app.get('/boom', async (req, res) => {
+  res.status(500).json({ message: "My bad" })
+})
+
+app.get('/players/salary', async (req, res) => {
+  res.status(403).send({
+    'error': 'Cannot access this information'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
